65e72da451cb9e8876d4d85651d749aa643b24fe
shakram02 <ahmedhamdyau@gmail.com>
2018-08-25 21:07:12 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from pynput.mouse import Button, Controller
+from time import sleep
+from threading import Thread
+import random
+
+
+def move_click_left(mouse, x, y):
+    mouse.position = (x, y)
+    mouse.click(Button.left)
+
+
+def prevent_lock(mouse, will_wait, move_delay=5.0):
+    """Moves the mouse cursor randomly so the screen doesn't lock"""
+    while will_wait():
+        mouse.position = (random.randint(0, 800), random.randint(0, 800))
+        sleep(move_delay)
+
+
+def main():
+    mouse = Controller()
+    delay_secs = 3
+    sleeper = Thread(target=sleep, args=(delay_secs,), daemon=True)
+    sleeper.start()
+
+    prevent_lock(mouse, sleeper.is_alive)
+
+    # Open the menu
+    move_click_left(mouse, 76, 71)
+
+    # Click resume download
+    # move_click_left(mouse, 81, 167)
+
+
+if __name__ == "__main__":
+    main()
